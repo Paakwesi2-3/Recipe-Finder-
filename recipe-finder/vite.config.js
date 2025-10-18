18f327af91272207be2f09097b58d11948050b7e
Paa Kwesi Larbi <paakwesilarbi2@gmail.com>
2025-10-18 06:29:30 +0000

    update

--- recipe-finder/vite.config.js
+++ recipe-finder/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
+  // Add this for proper base path
+  base: './'
 })
